fde767a2280e863f536339e0c97e3021e66eedc8
cgcheery27 <47472901+cgcheery27@users.noreply.github.com>
2022-07-01 17:50:14 +0530

    path of guestbook api

--- guestbook.js
+++ guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: `guestbook/read/`,
+      url: `https://8f420cdc.eu-gb.apigw.appdomain.cloud/guestbook/entries`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `guestbook/save/`,
+      url: `https://8f420cdc.eu-gb.apigw.appdomain.cloud/guestbook/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
